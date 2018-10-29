9e8f617803312168f0d80d31b4ad134d20f5c819
tanho <hbtan@tma.com.vn>
2018-10-29 16:51:31 +0700

    v5

---
+++ product-table/src/index.1.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';  
+import { createStore } from 'redux';
+import rootReducers from './reducers';
+// import App from './components/App';
+
+// const store = createStore(rootReducers)
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
+
+// class Hello extends React.Component {
+//     render() {
+//       return (
+//         <h1>
+//           xin chao
+//         </h1>
+//       );
+//     }
+//   }
+
+// // ========================================
+
+// ReactDOM.render(
+//   <Hello />,
+//   document.getElementById('root')
+// );
+  
--- product-table/src/index.js
+++ product-table/src/index.js
@@ -1,202 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TrackList from './components/TrackList';
-import {configureStore} from './store';
-import * as actions from './actions';
+// import { Provider } from 'react-redux';  
+import { createStore } from 'redux';
 
-const tracks = [
-  {
-    id: 1,
-    title: 'Em của ngày hôm qua'
-  },
-  {
-    id: 2,
-    title: 'Cơn mưa ngang qua'
-  }
-];
-
-const store = configureStore();
-store.dispatch(actions.setTracks(tracks));
-
-ReactDOM.render(
-  <TrackList />,
-  document.getElementById('root')
-);
-
-
-class ProductCategoryRow extends React.Component {
-    render() {
-      const category = this.props.category;
-      return (
-        <tr>
-          <th colSpan="2">
-            {category}
-          </th>
-        </tr>
-      );
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "INCREMENT": 
+            return state + action.payload;
+        case "DECREMENT":
+            return state - action.payload;
+        default: 
+            return state;
     }
-  }
-  
-  class ProductRow extends React.Component {
-    render() {
-      const product = this.props.product;
-      const name = product.stocked ?
-        product.name :
-        <span style={{color: 'red'}}>
-          {product.name}
-        </span>;
-  
-      return (
-        <tr>
-          <td>{name}</td>
-          <td>{product.price}</td>
-        </tr>
-      );
+}
+
+const initialState = 0;
+
+const store = createStore(reducer, initialState);
+
+store.subscribe(() => {
+    console.log("State has changed: " + store.getState());
+    document.getElementById('valueID').innerHTML = store.getState();
+})
+
+const incrementCount = (count) => {
+    return {
+        type: "INCREMENT",
+        payload: count
     }
-  }
-  
-  class ProductTable extends React.Component {
-    render() {
-      const filterText = this.props.filterText;
-      const inStockOnly = this.props.inStockOnly;
-  
-      const rows = [];
-      let lastCategory = null;
-  
-      this.props.products.forEach((product) => {
-        if (product.name.indexOf(filterText) === -1) {
-          return;
-        }
-        if (inStockOnly && !product.stocked) {
-          return;
-        }
-        if (product.category !== lastCategory) {
-          rows.push(
-            <ProductCategoryRow
-              category={product.category}
-              key={product.category} />
-          );
-        }
-        rows.push(
-          <ProductRow
-            product={product}
-            key={product.name}
-          />
-        );
-        lastCategory = product.category;
-      });
-  
-      return (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </table>
-      );
+}
+
+const decrementCount = (count) => {
+    return {
+        type: "DECREMENT",
+        payload: count
     }
-  }
-  
-  class SearchBar extends React.Component {
+}
+
+// store.dispatch(incrementCount(4));
+// store.dispatch(decrementCount(2));
+
+class Hello extends React.Component {
     constructor(props) {
-      super(props);
-      this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-      this.handleInStockChange = this.handleInStockChange.bind(this);
+        super(props);
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
     }
-    
-    handleFilterTextChange(e) {
-      this.props.onFilterTextChange(e.target.value);
+
+    increment() {
+        store.dispatch(incrementCount(1));
     }
-    
-    handleInStockChange(e) {
-      this.props.onInStockChange(e.target.checked);
+
+    decrement() {
+        store.dispatch(decrementCount(1));
     }
-    
-    render() {
-      return (
-        <form>
-          <input
-            type="text"
-            placeholder="Search..."
-            value={this.props.filterText}
-            onChange={this.handleFilterTextChange}
-          />
-          <p>
-            <input
-              type="checkbox"
-              checked={this.props.inStockOnly}
-              onChange={this.handleInStockChange}
-            />
-            {' '}
-            Only show products in stock
-          </p>
-        </form>
-      );
-    }
-  }
-  
-  class FilterableProductTable extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        filterText: '',
-        inStockOnly: false
-      };
-      
-      this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-      this.handleInStockChange = this.handleInStockChange.bind(this);
-    }
-  
-    handleFilterTextChange(filterText) {
-      this.setState({
-        filterText: filterText
-      });
-    }
-    
-    handleInStockChange(inStockOnly) {
-      this.setState({
-        inStockOnly: inStockOnly
-      })
-    }
-  
+
     render() {
       return (
         <div>
-          <SearchBar
-            filterText={this.state.filterText}
-            inStockOnly={this.state.inStockOnly}
-            onFilterTextChange={this.handleFilterTextChange}
-            onInStockChange={this.handleInStockChange}
-          />
-          <ProductTable
-            products={this.props.products}
-            filterText={this.state.filterText}
-            inStockOnly={this.state.inStockOnly}
-          />
+            <button onClick={this.increment}>+</button> <label id="valueID">0</label> <button onClick={this.decrement}>-</button>
         </div>
       );
     }
   }
-  
-  
-  const PRODUCTS = [
-    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-  ];
-  
 
 // ========================================
 
-// ReactDOM.render(
-//   <FilterableProductTable products={PRODUCTS} />,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('root')
+);
   

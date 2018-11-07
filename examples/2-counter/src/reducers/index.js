e23c47073ac4f711629ba6d3efe9a5a65194139f
ngo van bon <ngovanbon16@gmail.com>
2018-11-07 22:48:12 +0700

    v12

---
+++ examples/2-counter/src/reducers/index.js
@@ -0,0 +1,11 @@
+const initStart = 0;
+export default (state = initStart, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
